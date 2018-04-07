a6035daf3fe22174e68850a094d6c6563c04cce5
Jakub Piotrowski <jakub.piotrowski@lekta.ai>
2018-04-07 15:34:41 +0200

    Init commit. Data loader.

---
+++ data_loader.py
@@ -0,0 +1,58 @@
+import csv
+import datetime
+import json
+import pickle
+from os import path
+
+import quandl
+
+
+class DataLoader:
+
+    def __init__(self):
+        config = DataLoader._load_config()
+        quandl.ApiConfig.api_key = config['quandl_key']
+        tickers_path = config['tickers_path']
+        self.data_path = config['data_path']
+        self.tickers = ['WIKI/' + ticker for ticker in DataLoader._load_tickers(tickers_path)]
+        self.start_date = datetime.date(1990, 1, 1)
+        # self.start_date = datetime.date(2018, 3, 27)
+        self.end_date = datetime.datetime.now().date()
+
+    @staticmethod
+    def _load_config():
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+        return config
+
+    @staticmethod
+    def _load_tickers(tickers_path):
+        with open(tickers_path, 'r') as f:
+            tickers = list(csv.reader(f))[0]
+
+        return tickers
+
+    @staticmethod
+    def _load_pickle(filename):
+        if path.exists(filename):
+            print('Loading %s.' % filename)
+            return pickle.load(open(filename, 'rb'))
+        else:
+            return None
+
+    @staticmethod
+    def _save_pickle(data, filename):
+        print('Pickling %s.' % filename)
+        pickle.dump(data, open(filename, 'wb'), pickle.HIGHEST_PROTOCOL)
+        return data
+
+    def load(self):
+        data = DataLoader._load_pickle(self.data_path)
+        if data is None:
+            data = quandl.get(self.tickers, start_date=self.start_date, end_date=self.end_date)
+            DataLoader._save_pickle(data, self.data_path)
+        print('dupa')
+
+
+loader = DataLoader()
+loader.load()
