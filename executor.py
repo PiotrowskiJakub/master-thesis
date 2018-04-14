fd971bb5f2a053d834930bba2e724bea11e2d5e2
Jakub Piotrowski <jakub.piotrowski@lekta.ai>
2018-04-14 16:51:14 +0200

    Optimizer added

--- executor.py
+++ executor.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torch.autograd import Variable
 
 from data_loader import DataLoader
@@ -7,26 +8,28 @@ from model import Model
 from preprocessor import Preprocessor
 
 
-def train(model, loss, input_seq, target):
-    rnn.zero_grad()
+def train(optimizer, model, loss, input_seq, target):
+    optimizer.zero_grad()
 
     output = model(input_seq)
     err = loss(output, torch.nonzero(target).squeeze(1))
     err.backward()
+    optimizer.step()
 
-    return rnn, criterion, output, err.data[0]
+    return optimizer, model, criterion, output, err.data[0]
 
 
 if __name__ == '__main__':
-    rnn = Model(1, 128, 5)
+    model = Model(1, 128, 5)
     criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     data = DataLoader().load()
     preprocessor = Preprocessor(data)
     X, y = preprocessor.prepare_dataset()
     for idx, x in enumerate(X):
         x = Variable(torch.from_numpy(x).type(torch.FloatTensor)).view(x.size, 1, 1)
         out = Variable(torch.LongTensor(y[idx]))
-        rnn, criterion, output, loss = train(rnn, criterion, x, out)
+        optimizer, model, criterion, output, loss = train(optimizer, model, criterion, x, out)
         print(loss)
 
     print('All done')
--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class Model(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Model, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, 2)
+        self.lstm = nn.LSTM(input_size, hidden_size, 3)
         self.linear = nn.Linear(hidden_size, output_size)
 
     def forward(self, input_seq):
